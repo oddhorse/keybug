b185a5ffe27a2b2d5c91fe8edcb0472c9084aadd
oddhorse <15389659+oddhorse@users.noreply.github.com>
2026-07-20 16:22:37 -0400

    swift beginninges

---
+++ keybug/keybug/keybugApp.swift
@@ -0,0 +1,17 @@
+//
+//  keybugApp.swift
+//  keybug
+//
+//  Created by john on 7/20/26.
+//
+
+import SwiftUI
+
+@main
+struct keybugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
